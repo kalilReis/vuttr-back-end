147b70c1d604a1f4bf2432213b528910cce9c075
kalilReis <kalilreisdesisto@gmail.com>
2019-11-20 21:49:47 -0300

    fistname lastname added to User

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -1,16 +1,17 @@
 import { Schema, model, Document } from 'mongoose'
 
 interface UserI extends Document {
-  name: string,
+  firstName: string,
+  lastName: string,
   email: string,
   password: string
 }
 
 const UserSchema = new Schema({
-  name: String,
+  firstName: String,
+  lastName: String,
   email: String,
   password: String
-
 }, {
   timestamps: true
 })
